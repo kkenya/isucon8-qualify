975ad7868f6c1a0e17298c389652d40da3b1b9e8
Kenya Kawatsu <3982ne@gmail.com>
2019-09-01 18:34:52 +0000

    fix reload.sh

--- reload.sh
+++ reload.sh
@@ -4,11 +4,11 @@ set -ex
 if [ -f /var/lib/maria/slow.log ]; then
     sudo mv /var/log/mariadb/slow.log /var/log/mariadb/slow.log.$(date +%Y%m%d-%H%M%S)
 fi
-if [ -f /var/log/h2o/.access.log ]; then
-    sudo mv /var/log/h2o/access.log /var/h2o/access.log.$(date +%Y%m%d-%H%M%S)
+if [ -f /var/log/nginx/.access.log ]; then
+    sudo mv /var/log/nginx/access.log /var/nginx/access.log.$(date +%Y%m%d-%H%M%S)
 fi
 sudo systemctl restart mariadb
-sudo systemctl restart h2o
+sudo systemctl restart nginx
 sudo systemctl restart torb.nodejs.service
 # エラーがないか
 sudo journalctl -f
